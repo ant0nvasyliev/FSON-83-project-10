acf1a390f72d4bf3c3922e56f8d65bf653f805ba
ant0nvasyliev <anton12vasyliev@gmail.com>
2023-03-28 14:48:17 +0100

    slider-review js

---
+++ src/js/slider-review.js
@@ -0,0 +1,22 @@
+const swiper = new Swiper('.swiper', {
+  // Optional parameters
+  direction: 'horizontal',
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 10,
+  // If we need pagination
+  pagination: {
+    el: '.swiper-pagination-review',
+    clickable: true,
+  },
+  breakpoints: {
+    768: {
+      slidesPerView: 2,
+      spaceBetween: 16,
+    },
+    1200: {
+      slidesPerView: 3,
+      spaceBetween: 28,
+    },
+  },
+});
